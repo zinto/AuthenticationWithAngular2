e51d1a842bcd8e8495f922442155e44d033dc8ce
Nguyen Anh Dao <dao.nguyen@smoovapp.com>
2016-05-28 03:10:10 +0700

    init code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AuthenticationWithAngular2
+
+Base on https://auth0.com/blog/2015/05/14/creating-your-first-real-world-angular-2-app-from-authentication-to-calling-an-api-and-everything-in-between/
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// src/index.ts
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { provide } from '@angular/core';
+import { FORM_PROVIDERS } from '@angular/common';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
+import { AuthConfig, AuthHttp } from 'angular2-jwt';
+
+import { App } from './app/app';
+
+bootstrap(
+  App,
+  [
+    FORM_PROVIDERS,
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    provide(AuthHttp, {
+      useFactory: (http) => {
+        return new AuthHttp(new AuthConfig({
+          tokenName: 'jwt'
+        }), http);
+      },
+      deps: [Http]
+    })
+  ]
+);
